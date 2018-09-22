44ef0977e805839e9a9f7d4be7aff61d777b1aef
RedSparx* <RedSparx@users.noreply.github.com>
2018-09-22 11:26:30 -0400

    Changed comments to properly reflect objectives for this ADALINE (previously labeled a perceptron). Reoriented some of the data sets to provide some overlap between classes (to demonstrate misclassifications). Corrected target labels so that they are {+1,-1}. Changed the figure sizes and added proper titles to subplots. Produced a text plot with class labels and color highlighted misclassifications.

--- Lab4/L4_EX2_LMS_Noise_Cancellation.py
+++ Lab4/L4_EX2_LMS_Noise_Cancellation.py
@@ -1,21 +1,32 @@
-"""
-LMS Noise Cancellation
-----------------------
-
-Uses a gradient descent on the error surface to adjust weights for an adaptive filter.
-"""
-
 """LABORATORY 4
-EXERCISE #2: Train a perceptron to separate data into two classes.  Use the learned decision line to classify new data.
-    (a) Synthesize a pure sinusoid signal and corrupt it with noise (t={0..2pi}, f(x)= sin(5x).
-    (b) Initialize a Weiner filter of length k=500. 
-    (c) Recover the pure signal from the noisy data by adjusting filter weights adaptively.  Store the output.
-    (d) Plot the original signal, the learned noise signal, as well as the filter output.
-"""
+EXERCISE #2: NLMS Noise Cancellation. Recover a signal that has been corrupted by noise using an adaptive filter.
+Apply the filter to the input x and adjust its weights according to the error signal e (difference between the filter
+output and the desired output d).
 
+           ------------
+  x-----> |     h      |---------+------> y
+           ------------          |
+                ^                |
+                |  dw            |
+                |                |
+           ------------          V
+          | LMS Update | <------(+)<----- d
+           ------------    e=d-y
+
+   x = Microphone input.
+   d = Desired output (original signal).
+   y = Filter output.
+
+The difference between the filter output and the desired output is the error e=d-y.
+    (a) Synthesize a pure sinusoid signal and corrupt it with noise (t={0..2pi}, f(x)= sin(5x).
+    (b) Initialize the weight vector (w) for the filter (length k=500).
+    (c) Initialize output and error arrays to hold these values for each shift of the filter through the data.
+    (d) Recover the pure signal from the noisy data by adjusting filter weights adaptively using the NLMS learning rule.
+        Store the output and error signals.
+    (e) Plot the original signal, the learned noise signal, as well as the filter output.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
-
 # region Synthesize a signal in the time domain (d) and then corrupt it with noise (n).
 N = 10000
 t = np.linspace(0, 2*np.pi, N)
@@ -24,36 +35,17 @@ n = np.random.normal(0, 0.5, N)
 # n = 0.1*np.cos(3*t)**2
 x = d + n
 # endregion
-# region Initialize the filter (of length k). Apply the filter to the input x and adjust its weights according to the
-# error signal e (difference between the filter output and the desired output d).
-#
-#
-#            ------------
-#   x-----> |     h      |---------+------> y
-#            ------------          |
-#                 ^                |
-#                 |  dw            |
-#                 |                |
-#            ------------          V
-#           | LMS Update | <------(+)<----- d
-#            ------------    e=d-y
-#
-#    x = Microphone input.
-#    d = Desired output (original signal).
-#    y = Filter output.
-#
-# The difference between the filter output and the desired output is the error e=d-y.
-
-# Setup filter parameters and randomize the weights.
+# region Initialize the filter (of length k). Randomize the weights.
 k = 500
 mu = 1E-1
 w = np.zeros(k)
-
-# Initialize the output.
+# endregion
+# region Initialize output and error arrays to hold these values for each shift of the filter through the data.
 y = np.ones(N)*np.nan
 Err = np.zeros(N)
-
-# Apply the filter.
+# endregion
+# region Recover the pure signal from the noisy data by adjusting filter weights adaptively using the NLMS learning rule.
+# Store the output and error signals.
 for n in range(N-k):
     y[n] = np.dot(w, x[n:(n + k)])
     # e = d[n:(n + k)] - y[n:(n + k)]
@@ -61,7 +53,6 @@ for n in range(N-k):
     Err[n] = e
     eta = mu/(np.linalg.norm(x[n:(n+k)])**2)
     w = w + 2 * eta * e * x[n:(n+k)]
-
 # endregion
 # region Produce plots.
 plt.subplot(1, 2, 1)
